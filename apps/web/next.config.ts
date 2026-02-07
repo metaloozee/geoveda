1ad14e37019a5a2c2e7c8a1bd1c0dd6e1488df10
metaloozee <ayanparkar123@gmail.com>
2026-02-07 22:00:00 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@geoveda/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
