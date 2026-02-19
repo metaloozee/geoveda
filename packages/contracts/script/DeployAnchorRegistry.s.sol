1d698aa6023648013a2c6f2ec9bc3d78154ba368
metaloozee <ayanparkar123@gmail.com>
2026-02-19 12:50:02 +0530

    fix: format

--- packages/contracts/script/DeployAnchorRegistry.s.sol
+++ packages/contracts/script/DeployAnchorRegistry.s.sol
@@ -5,11 +5,11 @@ import "forge-std/Script.sol";
 import "../src/AnchorRegistry.sol";
 
 contract DeployAnchorRegistry is Script {
-    function run() external returns (AnchorRegistry deployed) {
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+  function run() external returns (AnchorRegistry deployed) {
+    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-        vm.startBroadcast(deployerPrivateKey);
-        deployed = new AnchorRegistry();
-        vm.stopBroadcast();
-    }
+    vm.startBroadcast(deployerPrivateKey);
+    deployed = new AnchorRegistry();
+    vm.stopBroadcast();
+  }
 }
--- packages/contracts/test/AnchorRegistry.t.sol
+++ packages/contracts/test/AnchorRegistry.t.sol
@@ -5,52 +5,47 @@ import "forge-std/Test.sol";
 import "../src/AnchorRegistry.sol";
 
 contract AnchorRegistryTest is Test {
-    AnchorRegistry internal registry;
+  AnchorRegistry internal registry;
 
-    event Anchored(
-        bytes32 indexed dataHash,
-        bytes32 indexed stepKey,
-        address indexed actor,
-        uint256 timestamp
-    );
+  event Anchored(
+    bytes32 indexed dataHash, bytes32 indexed stepKey, address indexed actor, uint256 timestamp
+  );
 
-    function setUp() public {
-        registry = new AnchorRegistry();
-    }
+  function setUp() public {
+    registry = new AnchorRegistry();
+  }
 
-    function test_anchorStep_emitsAnchoredEvent() public {
-        bytes32 dataHash = keccak256("data");
-        bytes32 stepKey = keccak256("step-1");
+  function test_anchorStep_emitsAnchoredEvent() public {
+    bytes32 dataHash = keccak256("data");
+    bytes32 stepKey = keccak256("step-1");
 
-        vm.expectEmit(true, true, true, false);
-        emit Anchored(dataHash, stepKey, address(this), 0);
+    vm.expectEmit(true, true, true, false);
+    emit Anchored(dataHash, stepKey, address(this), 0);
 
-        registry.anchorStep(dataHash, stepKey);
-        assertTrue(registry.anchoredStepKeys(stepKey));
-    }
+    registry.anchorStep(dataHash, stepKey);
+    assertTrue(registry.anchoredStepKeys(stepKey));
+  }
 
-    function test_anchorStep_revertsOnZeroHash() public {
-        bytes32 stepKey = keccak256("step-1");
-        vm.expectRevert(AnchorRegistry.ZeroHash.selector);
-        registry.anchorStep(bytes32(0), stepKey);
-    }
+  function test_anchorStep_revertsOnZeroHash() public {
+    bytes32 stepKey = keccak256("step-1");
+    vm.expectRevert(AnchorRegistry.ZeroHash.selector);
+    registry.anchorStep(bytes32(0), stepKey);
+  }
 
-    function test_anchorStep_revertsOnZeroStepKey() public {
-        bytes32 dataHash = keccak256("data");
+  function test_anchorStep_revertsOnZeroStepKey() public {
+    bytes32 dataHash = keccak256("data");
 
-        vm.expectRevert(AnchorRegistry.ZeroHash.selector);
-        registry.anchorStep(dataHash, bytes32(0));
-    }
+    vm.expectRevert(AnchorRegistry.ZeroHash.selector);
+    registry.anchorStep(dataHash, bytes32(0));
+  }
 
-    function test_anchorStep_revertsOnDuplicateStepKey() public {
-        bytes32 dataHash = keccak256("data");
-        bytes32 stepKey = keccak256("step-1");
+  function test_anchorStep_revertsOnDuplicateStepKey() public {
+    bytes32 dataHash = keccak256("data");
+    bytes32 stepKey = keccak256("step-1");
 
-        registry.anchorStep(dataHash, stepKey);
+    registry.anchorStep(dataHash, stepKey);
 
-        vm.expectRevert(
-            abi.encodeWithSelector(AnchorRegistry.AlreadyAnchored.selector, stepKey)
-        );
-        registry.anchorStep(dataHash, stepKey);
-    }
+    vm.expectRevert(abi.encodeWithSelector(AnchorRegistry.AlreadyAnchored.selector, stepKey));
+    registry.anchorStep(dataHash, stepKey);
+  }
 }
